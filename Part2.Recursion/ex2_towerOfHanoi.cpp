09467f96380673b43e08b5ea9ea4edc26036a224
arjun <arjun.z.tang@gmail.com>
2022-05-24 16:07:30 +0800

    1

---
+++ Part2.Recursion/ex2_towerOfHanoi.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void hanoi(int n,char src,char mid,char dest,int & counts){
+    if(n==1){
+        cout<<counts<<"."<<src<<"->"<<dest<<endl;
+        counts++;
+        return;
+    }
+    hanoi(n-1,src,dest,mid,counts);
+    cout<<counts<<"."<<src<<"->"<<dest<<endl;
+    counts++;
+    hanoi(n-1,mid,src,dest,counts);
+    return;
+}
+
+int main(){
+    int n,counts;
+    cin>>n;
+    hanoi(n,'A','B','C',counts);
+    cout<<"Total moves: "<<counts-1<<endl;
+    return 0;
+}
