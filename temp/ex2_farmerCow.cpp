8b92124ff71ba783f06bf105b613bdf5a000a219
ArjunTang <arjun.z.tang@gmail.com>
2022-05-30 13:00:02 +0800

    1

--- temp/ex2_farmerCow.cpp
+++ temp/ex2_farmerCow.cpp
@@ -1,3 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int f1(int x[],int n){
+    //排序
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(x[i]>x[j]){
+                int temp=x[i];
+                x[i]=x[j];
+                x[j]=temp;
+            }
+        }
+    }
+    
+}
