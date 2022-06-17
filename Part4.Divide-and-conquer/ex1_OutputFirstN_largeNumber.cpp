26844843e6e49af7c7e0d6a446b4294472922ea6
arjun <arjun.z.tang@gmail.com>
2022-06-17 14:34:54 +0800

    1

--- Part4.Divide-and-conquer/ex1_OutputFirstN_largeNumber.cpp
+++ Part4.Divide-and-conquer/ex1_OutputFirstN_largeNumber.cpp
@@ -19,7 +19,78 @@
     若m足够小则可以使时间复杂度降至O(n),下面使用分治处理.
 */
 
-//把数组前k大的都弄到最右边
-void arrangeRight(){
+#include<iostream>
+using namespace std;
 
+void swap(int &a,int &b){
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void QuickSort(int a[],int s,int e){
+    if(s>=e){
+        return;
+    }
+    int i = s,j = e;
+    int pivot = a[(s+e)/2];
+    while(i<=j){
+        while(a[i]<pivot){
+            i++;
+        }
+        while(a[j]>pivot){
+            j--;
+        }
+        if(i<=j){
+            swap(a[i],a[j]);
+            i++;
+            j--;
+        }
+    }
+    QuickSort(a,s,j);
+    QuickSort(a,i,e);
+}
+
+//把数组前k大的都弄到最右边
+void arrangeRight(int a[],int left,int right,int k){
+    if(left>=right){
+        return;
+    }
+    if(k==right-left+1){
+        return;
+    }
+    int i = left,j = right;
+    int pivot = a[left];
+    while(i != j){
+        while(i<j && a[j]>=pivot){
+            j--;
+        }
+        swap(a[i],a[j]);
+        while(i<j && a[i]<=pivot){
+            i++;
+        }
+        swap(a[i],a[j]);
+    }
+    if(j-i+1==k){
+        return;
+    }
+    else if(j-i+1>k){
+        arrangeRight(a,i+1,right,k);
+    }
+    else{
+        arrangeRight(a,left,i-1,k-j+i-1);
+    }
+}
+
+int main(){
+    int a[]={11, 4, 14, 6, 1, 12, 8, 9, 3, 13, 7, 10, 5, 2, 15};
+    int size = sizeof(a)/sizeof(a[0]);
+    int k = 5;
+    arrangeRight(a,0,size-1,k);
+    QuickSort(a,size-k,size-1);
+    while(k--){
+        cout<<a[--size]<<" ";
+    }
+    cout<<endl;
+    return 0;
 }
